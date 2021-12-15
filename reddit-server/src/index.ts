37a0a98eac453347a16ac459614deeff59b9b9a8
Hamza Fouad <HamzaMoFouad@gmail.com>
2021-12-15 05:15:08 +0200

    Setup MikroORM with initial post commit

--- reddit-server/src/index.ts
+++ reddit-server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello!!!")
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  console.log("dirname: ", __dirname);
+
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  const post = orm.em.create(Post, { title: "My First Post" });
+  await orm.em.persistAndFlush(post);
+};
+
+main().catch((err) => {
+  console.log(err);
+});
---
+++ reddit-server/src/mikro-orm.config.ts
@@ -0,0 +1,21 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(path.join(__dirname, "./migrations"));
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  debug: !__prod__,
+  type: "postgresql",
+  dbName: `${process.env.DB}`,
+  user: `${process.env.POSTGRESQL_USERNAME}`,
+  password: `${process.env.POSTGRESQL_PASSWORD}`,
+} as Parameters<typeof MikroORM.init>[0];
