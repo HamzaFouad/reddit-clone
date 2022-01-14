4d0b201b5825639a3ed75b1f6f962f75ab1e886c
Hamza Fouad <HamzaMoFouad@gmail.com>
2022-01-14 18:32:04 +0200

    Add prettier on save for the UI

--- reddit-web/src/pages/register.tsx
+++ reddit-web/src/pages/register.tsx
@@ -1,29 +1,54 @@
-import React from 'react';
-import {Formik, Form} from 'formik';
-import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
-import { Wrapper } from '../components/Wrapper';
-import { InputField } from '../components/InputField';
+import React from "react";
+import { Formik, Form } from "formik";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
 
 interface registerProps {}
 
-
 const Register: React.FC<registerProps> = ({}) => {
   return (
-    <Wrapper variant='small'>
-      <Formik initialValues={{username: "", password: ""}} onSubmit={(values) => console.log(values)}>
-        { ({isSubmitting}) => (
-            <Form>
-              <InputField name='username' placeholder="username" label="username"></InputField>
-              <Box mt={4}>
-                <InputField name='password' placeholder="password" label="password" type="password"></InputField>
-              </Box>
-              <Button mt={4} type='submit' colorScheme="teal" isLoading={isSubmitting}> Register </Button>
-            </Form>
-          )
-        }
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="username"
+            ></InputField>
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="password"
+                type="password"
+              ></InputField>
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              colorScheme="teal"
+              isLoading={isSubmitting}
+            >
+              {" "}
+              Register{" "}
+            </Button>
+          </Form>
+        )}
       </Formik>
     </Wrapper>
-  )
-}
+  );
+};
 
 export default Register;
