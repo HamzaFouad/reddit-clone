9d6885b23cbddddadba8f199937df1f0282f8ed9
Hamza Fouad <HamzaMoFouad@gmail.com>
2022-01-16 00:17:27 +0200

    Initialize Me.GraphQL, user profile, when logged in or registered

--- reddit-web/src/components/navBar.tsx
+++ reddit-web/src/components/navBar.tsx
@@ -1,13 +1,19 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+  if (fetching) {
+    // data is loading -- body is null
+  } else if (!data?.me) {
+    // user not logged in
+    body = (
+      <>
         <NextLink href="/login">
           <Link color="#FFFFFF" mr={2}>
             Login
@@ -16,7 +22,22 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         <NextLink href="/register">
           <Link color="#FFFFFF">Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    // user is logged in
+    body = (
+      <Flex>
+        <Box color="#FFFFFF" mr={2}>
+          {data.me.username}
+        </Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
 };
